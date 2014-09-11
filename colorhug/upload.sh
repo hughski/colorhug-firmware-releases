da56fe78a0873783d57d7c669a82c1bfea1507c3
Richard Hughes <richard@hughsie.com>
2014-09-11 22:14:41 +0100

    Upload the first ColorHug2 firmware version and metadata

---
+++ colorhug/upload.sh
@@ -0,0 +1,1 @@
+scp -P 3784 *.bin manifest.xml hughsiec@hughski.com:~/public_html/hughski/downloads/colorhug/firmware/
---
+++ colorhug2/upload.sh
@@ -0,0 +1,1 @@
+scp -P 3784 *.bin metadata.xml hughsiec@hughski.com:~/public_html/hughski/downloads/colorhug2/firmware/
